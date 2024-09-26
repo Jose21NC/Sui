6256f12321c0a90c864cade80684d9ce1aeb15a2
José <84270806+Jose21NC@users.noreply.github.com>
2024-09-25 20:31:22 -0600

    UPDATE main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'auth_service.dart'; 
+import 'presentation/screens/login_screen.dart'; 
+import 'presentation/screens/register_screen.dart'; 
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MainApp extends StatelessWidget { // MainApp
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(create: (_) => AuthService()), // Servicio de autenticación
+      ],
+      child: MaterialApp(
+        title: 'Sui App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
+        initialRoute: '/login', // La ruta inicial será la pantalla de login
+        routes: {
+          '/login': (context) => LoginScreen(), // Pantalla de login
+          '/register': (context) => RegisterScreen(), // Pantalla de registro
+          '/home': (context) => Scaffold(body: Center(child: Text('Pantalla Principal'))), // Placeholder para la pantalla principal
+        },
       ),
     );
   }
