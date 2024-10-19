f1d5fad50da879daf7bf02a0228074bc7d63b3d9
Jose21NC <josealonzo121@gmail.com>
2024-10-19 00:48:03 -0600

    HOME SCREEN

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,48 +1,53 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'login_screen.dart'; // Importar tu pantalla de inicio de sesión
 
-class SplashScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Temporizador para cambiar a la pantalla de inicio de sesión después de 3 segundos
-    Timer(Duration(seconds: 3), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => LoginScreen())
-      );
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _pages = <Widget>[
+    // Aquí podrías agregar las diferentes pantallas para las secciones
+    Center(child: Text('Inicio', style: TextStyle(fontSize: 30))),
+    Center(child: Text('Actividad Física', style: TextStyle(fontSize: 30))),
+    Center(child: Text('Perfil', style: TextStyle(fontSize: 30))),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // Puedes cambiar el color de fondo según tu preferencia
+      appBar: AppBar(
+        title: Text('Sui - Tu Salud'),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo_sui.png', // Asegúrate de que la imagen del logo esté en esta ruta
-              width: 200, // Tamaño de la imagen
-              height: 200,
-            ),
-            SizedBox(height: 20), // Espacio entre el logo y el texto
-            Text(
-              'Bienvenido a Sui', // Texto debajo del logo
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.blueAccent, // Personaliza los colores
-              ),
-            ),
-          ],
-        ),
+        child: _pages.elementAt(_selectedIndex), // Muestra la página seleccionada
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run),
+            label: 'Actividad',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blueAccent,
+        onTap: _onItemTapped,
       ),
     );
   }
